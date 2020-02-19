251fa36a691642588c8e042c1cad6556074fcc97
GABRAH <hb1115@york.ac.uk>
2020-02-19 14:51:06 +0000

    Alpha version of my analysis routine of Glycoprotein glycan sequences in the PDB

---
+++ carb_sequence_checker.py
@@ -0,0 +1,79 @@
+import os
+import csv
+import shutil
+import re
+import requests
+import privateer
+from datetime import datetime
+
+
+def GetDirectories(currentdirectory):
+    directories = []
+    for directory in os.listdir(currentdirectory):
+        if os.path.isdir(os.path.join(currentdirectory, directory)):
+            directories.append(directory)
+    return directories
+
+def CreateFolder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        shutil.rmtree(path)           # Removes all the subdirectories!
+        os.makedirs(path)
+
+
+
+
+tock = datetime.now()
+
+rootDir = os.getcwd()
+structureListDir = os.path.join(rootDir, "structures")
+srcFileDirs = GetDirectories(structureListDir)
+resultsDir = os.path.join(rootDir, "results")
+
+
+for directory in srcFileDirs:
+    sourceDir = os.path.join(structureListDir, directory)
+    outputDir = os.path.join(resultsDir, directory)
+    CreateFolder(outputDir)
+    with open(os.path.join(outputDir, "output.csv"), "a", newline="") as csvfile:
+        fieldnames = ["pdbID", "Residue", "Chain", "ClientWURCS", "ServerWURCS", "stringMatch"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({"pdbID": "PDB_ID", "Residue": "Residue", "Chain": "Chain", "ClientWURCS": "privateerWURCS", "ServerWURCS": "glycosmosWURCS", "stringMatch": "stringMatch"})
+        for count, pdbFile in enumerate(os.listdir(sourceDir)):
+            if pdbFile.endswith(".pdb"):
+                pdbID = os.path.splitext(os.path.basename(pdbFile))[0]
+                print(f'Currently processing: {pdbID}\nProgress: {count} out of {len(os.listdir(sourceDir))}\t Progress - {int((count/len(os.listdir(sourceDir))*100))}%')
+                pdbFilePath = os.path.join(sourceDir, pdbFile)
+                totalWURCS = privateer.print_wurcs(pdbFilePath)
+                
+                temporaryString = totalWURCS.split('\n', 1)[0]
+                if temporaryString[0:21] == 'Total Glycans Found: ':
+                    totalGlycansInModel = int(temporaryString[21:])
+                
+                temporaryListOfStrings = totalWURCS.splitlines()
+                temporaryListOfStrings = temporaryListOfStrings[1:]
+
+                for i in range(totalGlycansInModel):
+                    residueInfo = temporaryListOfStrings[0].partition("/")[0]
+                    chainInfo = temporaryListOfStrings[0].partition("/")[2]
+                    privateerWURCS = temporaryListOfStrings[1]
+
+                    queryLink = 'https://api.glycosmos.org/glytoucan/sparql/wurcs2gtcids?wurcs=' + privateerWURCS
+                    serverResponse = requests.get(queryLink).json()
+                    for item in serverResponse:
+                        glycosmosWURCS = item['WURCS']
+
+
+                    stringMatch = "TRUE" if privateerWURCS == glycosmosWURCS else "FALSE"
+                    
+                    writer.writerow({"pdbID": pdbID, "Residue": residueInfo, "Chain": chainInfo, "ClientWURCS": privateerWURCS, "ServerWURCS": glycosmosWURCS, "stringMatch": stringMatch})
+                    # print(pdbID, residueInfo, chainInfo, privateerWURCS, glycosmosWURCS, stringMatch)
+                    # "pdbID", "Residue", "Chain", "ClientWURCS", "ServerWURCS", "stringMatch"
+
+                    temporaryListOfStrings = temporaryListOfStrings[2:]
+tick = datetime.now()
+diff = tick - tock
+print(f'\nFinished analysing Glycan sequences of all models in root/structures directory!\nTime elapsed: {diff.total_seconds()} seconds')
+
+            
---
+++ pdb-downloader.py
@@ -0,0 +1,52 @@
+import os
+import csv
+import shutil
+import pypdb as pdb
+import privateer
+
+# function to create/delete existing folders
+def CreateFolder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        shutil.rmtree(path)           # Removes all the subdirectories!
+        os.makedirs(path)
+
+def WriteFile(directory, filename, content):
+    with open(os.path.join(directory,filename), "w") as file:
+        file.write(content)
+
+# function that generates an array of pdb ids from input file
+def GenerateListOfStructures(filepath, filename):
+    structureList = []
+    with open(os.path.join(filepath, filename)) as csv_file:
+        next(csv_file) # skip header line
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            structureList.append(row[1])
+    return structureList
+
+
+rootDir = os.getcwd()
+structureListDir = os.path.join(rootDir, "structures")
+
+
+for structureListFile in os.listdir(structureListDir):
+    if structureListFile.endswith(".csv"):
+        structureListFileName = os.path.splitext(os.path.basename(structureListFile))[0]
+        outputDir = os.path.join(structureListDir, structureListFileName)
+        CreateFolder(outputDir)
+        currentStructureList = GenerateListOfStructures(structureListDir, structureListFile)
+        for count, pdbID in enumerate(currentStructureList):
+            print(f'Currently downloading: {pdbID}\nProgress: {count} out of {len(currentStructureList)}\t Progress - {int((count/len(currentStructureList)*100))}%')
+            newFileName = pdbID + '.pdb'
+            currentPDB = pdb.get_pdb_file(pdbID, filetype='pdb', compression=False)
+            WriteFile(outputDir, newFileName, currentPDB)
+       
+
+
+
+
+
+
+
